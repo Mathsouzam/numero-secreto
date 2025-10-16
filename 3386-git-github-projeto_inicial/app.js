475edd8834442ef250adb22ca2b567d39f4708a2
Matheus Souza Melo <matheusouza9779@gmail.com>
2025-10-16 13:20:08 -0300

    testando

--- 3386-git-github-projeto_inicial/app.js
+++ 3386-git-github-projeto_inicial/app.js
@@ -36,6 +36,7 @@ function verificarChute() {
     }
 }
 //teste
+//tewkoesajod
 function gerarNumeroAleatorio() {
     let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
     let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
